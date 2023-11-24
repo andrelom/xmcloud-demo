f42748119e1ff3974231a1b201ab84a4dd8131f0
André Moraes <andrelom@gmail.com>
2023-11-24 17:31:33 -0300

    Save

--- src/Rendering/Demo/src/renderings/Footer.tsx
+++ src/Rendering/Demo/src/renderings/Footer.tsx
@@ -3,7 +3,7 @@ import type { GetStaticComponentProps } from '@sitecore-jss/sitecore-jss-nextjs'
 import Footer from '@/components/Footer'
 
 export const getStaticProps: GetStaticComponentProps = async (rendering, layout, context) => {
-  return import('@/components/Banner/getStaticProps').then((getStaticProps) => {
+  return import('@/components/Header/getStaticProps').then((getStaticProps) => {
     return getStaticProps.default(rendering, layout, context)
   })
 }
--- src/Rendering/Demo/src/renderings/Header.tsx
+++ src/Rendering/Demo/src/renderings/Header.tsx
@@ -3,7 +3,7 @@ import type { GetStaticComponentProps } from '@sitecore-jss/sitecore-jss-nextjs'
 import Header from '@/components/Header'
 
 export const getStaticProps: GetStaticComponentProps = async (rendering, layout, context) => {
-  return import('@/components/Banner/getStaticProps').then((getStaticProps) => {
+  return import('@/components/Header/getStaticProps').then((getStaticProps) => {
     return getStaticProps.default(rendering, layout, context)
   })
 }
