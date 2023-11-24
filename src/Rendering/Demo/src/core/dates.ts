981e92dc9e7b8529b48871bbf92d6dd79532a74b
André Moraes <andrelom@gmail.com>
2023-11-24 18:28:46 -0300

    Create dates.ts

---
+++ src/Rendering/Demo/src/core/dates.ts
@@ -0,0 +1,14 @@
+export function createUtcDate() {
+  const date = new Date()
+
+  return new Date(
+    Date.UTC(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate(),
+      date.getUTCHours(),
+      date.getUTCMinutes(),
+      date.getUTCSeconds(),
+    ),
+  )
+}
